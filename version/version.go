a2a0c5ac518469c133505688b1e8574e43a67e59
Zach <15034943+zricethezav@users.noreply.github.com>
2026-02-18 16:06:16 -0600

    min version checks for both gitleaks and betterleaks

--- version/version.go
+++ version/version.go
@@ -3,3 +3,7 @@ package version
 // these two gotta be the same
 var DefaultMsg = "dev"
 var Version = "dev"
+
+// GitleaksCompat is the gitleaks config format version this build supports.
+// Checked against the minVersion field in config files.
+const GitleaksCompat = "8.25.0"
