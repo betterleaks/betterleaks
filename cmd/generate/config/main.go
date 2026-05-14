a4e15424b3583c566b72bf07d78ee11b8e24f81b
PachkaKofe04 <ilchuk1996@gmail.com>
2026-05-14 10:51:05 +0500

    fix: register missing TrelloAccessToken rule

--- cmd/generate/config/main.go
+++ cmd/generate/config/main.go
@@ -322,6 +322,7 @@ func main() {
 		rules.TogetherAI(),
 		rules.TelegramBotToken(),
 		rules.TravisCIAccessToken(),
+		rules.TrelloAccessToken(),
 		rules.Twilio(),
 		rules.TwitchAPIToken(),
 		rules.TwitterAPIKey(),
