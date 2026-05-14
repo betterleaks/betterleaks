48570e45ccd4ec9bd9ec1e5255e40a62095cf2c9
Zach <15034943+zricethezav@users.noreply.github.com>
2026-05-14 10:31:49 -0500

    simple guard

--- detect/detect.go
+++ detect/detect.go
@@ -787,13 +787,16 @@ func (d *Detector) detectFragmentWithRule(fragment sources.Fragment,
 
 		finding.SetFingerprint()
 
-		finding.CELContext = extractContext(fragment.Raw, matchIndex, MatchContextSpec{
-			Mode:        ContextModeBox,
-			LinesBefore: 20,
-			LinesAfter:  20,
-			ColsBefore:  350,
-			ColsAfter:   350,
-		})
+		// before setting CELContext, check if we have a validate or filter (TODO rename CelProgram to ValidateProgram)
+		if r.CelProgram() != nil || d.Config.FilterProgram() != nil || r.FilterProgram() != nil {
+			finding.CELContext = extractContext(fragment.Raw, matchIndex, MatchContextSpec{
+				Mode:        ContextModeBox,
+				LinesBefore: 20,
+				LinesAfter:  20,
+				ColsBefore:  350,
+				ColsAfter:   350,
+			})
+		}
 
 		// Build finding map once, only when at least one filter program is compiled.
 		var findingMap map[string]string
