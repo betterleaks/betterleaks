9830abf03ec395070d71f01ea52d25e3dae7faec
Zach <15034943+zricethezav@users.noreply.github.com>
2026-07-21 11:13:43 -0500

    fixt

--- cmd/generate/config/rules/mercury.go
+++ cmd/generate/config/rules/mercury.go
@@ -11,10 +11,10 @@ func MercuryProductionAPIToken() *config.Rule {
 	r := config.Rule{
 		RuleID:      "mercury-production-api-token",
 		Description: "Mercury production API token.",
-		Regex:       utils.GenerateUniqueTokenRegex(`secret-token:mercury_production_[a-z]{3,6}_[A-Za-z0-9]{40,50}_yrucrem`, false),
+		Regex:       utils.GenerateUniqueTokenRegex(`mercury_production_[a-z]{3,6}_[A-Za-z0-9]{40,50}_yrucrem`, false),
 		Keywords:    []string{"mercury_production_"},
 		ValidateExpr: `let r = http.get("https://api.mercury.com/api/v1/accounts", {
-    "Authorization": "Bearer " + finding["secret"],
+    "Authorization": "Bearer secret-token:" + finding["secret"],
     "Accept": "application/json"
   }); r.status == 200 ? {
     "result": "valid"
@@ -27,11 +27,11 @@ func MercuryProductionAPIToken() *config.Rule {
 
 	// validate
 	tps := []string{
-		"MERCURY_API_TOKEN=secret-token:mercury_production_wma_" + secrets.NewSecretWithEntropy(utils.AlphaNumeric("44"), 3.5) + "_yrucrem",
+		"Authorization: Bearer secret-token:mercury_production_wma_" + secrets.NewSecretWithEntropy(utils.AlphaNumeric("44"), 3.5) + "_yrucrem",
 	}
 	fps := []string{
-		`MERCURY_API_TOKEN=mercury_production_wma_24SCp4G81X3yHL4Wq8FgzuaP9ye3VKf2mgTDctXyRg5HY_yrucrem`,
-		`MERCURY_API_TOKEN=secret-token:mercury_production_wma_short_yrucrem`,
+		`MERCURY_API_TOKEN=mercury_production_wma_short_yrucrem`,
+		`MERCURY_API_TOKEN=mercury_sandbox_rma_24pnbcT7NygLbpJPr4xBuSuBDpo6tK89S8u3ERYn3FXVz_yrucrem`,
 	}
 	return utils.Validate(r, tps, fps)
 }
